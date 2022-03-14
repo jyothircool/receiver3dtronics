927d82e1319c92715b7537e7969afacd6869cbaa
jyothircool <85424481+jyothircool@users.noreply.github.com>
2022-03-14 23:00:57 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    let people = 0
+    let _12 = 0
+    let _11 = 0
+    let _10 = 0
+    let _9 = 0
+    let _8 = 0
+    let _7 = 0
+    let _6 = 0
+    let _5 = 0
+    let _4 = 0
+    let _3 = 0
+    let _2 = 0
+    let _1 = 0
+    if (_1 + (_2 + (_3 + (_4 + (_5 + (_6 + (_7 + (_8 + (_9 + (_10 + (_11 + _12)))))))))) == 5) {
+        basic.showNumber(people)
+        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+    } else {
+        basic.showNumber(people)
+    }
+})
+basic.forever(function () {
+    if (true) {
+    	
+    }
+})
